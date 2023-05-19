d48e5e339d0395b3d19d9174009682192c5baafe
Leonardo Rodrigues <leonardo.rc999@gmail.com>
2023-05-19 19:08:24 -0300

    finalização formulario cadastro personagens

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import PersonsForm from './persons/form/PersonsForm';
 function App() {
   return (
     <div className='principal'>
+      <img className='sw-logo' src="./assets/imgs/logo.png" alt="logo" />
+
       < PersonsForm />
     </div>
   );
