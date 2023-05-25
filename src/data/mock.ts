f65be89543c0c9fc9abf4e0fbe0029eef8d112a2
Leonardo Rodrigues Cardozo <leonardo.rc999@gmail.com>
2023-05-24 21:13:29 -0300

    Estilização da tabela personagens e criação do método 'DELETE'.

--- src/data/mock.ts
+++ src/data/mock.ts
@@ -1,7 +1,26 @@
-const DATA_MOCK = [{
-    id: 1,
-    name: 'Luke Skywalker',
-    gender: 'Masculino'
-}]
+import { Data } from "../persons/list/PersonsList"
+
+const DATA_MOCK: Array<Data> = [
+    {
+        id: 1,
+        name: 'Luke Skywalker',
+        gender: 'Masculino'
+    },
+    {
+        id: 2,
+        name: 'Anakin Skywalker',
+        gender: 'Masculino'
+    },
+    {
+        id: 2,
+        name: 'Anakin Skywalker',
+        gender: 'Masculino'
+    },
+    {
+        id: 2,
+        name: 'Anakin Skywalker',
+        gender: 'Masculino'
+    }
+]
 
 export default DATA_MOCK
--- src/persons/list/PersonsList.tsx
+++ src/persons/list/PersonsList.tsx
@@ -2,8 +2,11 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './PersonsList.scss';
 import DATA_MOCK from '../../data/mock';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-interface Data {
+export interface Data {
     id: number;
     name: string;
     gender: string;
@@ -30,29 +33,53 @@ function PersonsList() {
         }
     };
 
+    const deleteItem = async (id: number) => {
+        try {
+            const response = await axios.delete(`http://localhost:3030/person/${id}`);
+
+            if (response.status === 200) {
+                alert('Dado excluído com sucesso!');
+                // Atualizar a lista de dados após a exclusão
+                fetchData();
+            } else {
+                alert('Erro ao excluir o dado.');
+            }
+        } catch (error) {
+            alert('Erro ao excluir o dado.');
+            console.error('Erro ao excluir o dado:', error);
+        }
+    };
+
     return (
         <div className="data-list">
-            <h2>Lista de Dados</h2>
+            <h2 className='title-table'>Lista de Personagens</h2>
             {DATA_MOCK.length > 0 ? (
-                <table>
+                <table className='persons-table'>
                     <thead>
                         <tr>
-                            <th>Nome</th>
-                            <th>Gênero</th>
+                            <th className='item-title'>Nome</th>
+                            <th className='item-title'>Gênero</th>
+                            <th className='item-title'></th>
+                            <th className='item-title'></th>
                         </tr>
                     </thead>
                     <tbody>
                         {DATA_MOCK.map((data) => (
-                            <tr key={data.id}>
-                                <td>{data.name}</td>
-                                <td>{data.gender}</td>
+                            <tr className='item' key={data.id}>
+                                <td className='item-info'>{data.name}</td>
+                                <td className='item-info'>{data.gender}</td>
+                                <td className='item-edit'> <FontAwesomeIcon icon={faPenToSquare} /> </td>
+                                <td className='item-delete'>
+                                    <FontAwesomeIcon icon={faTrash} onClick={() => deleteItem(data.id)} />
+                                </td>
                             </tr>
                         ))}
                     </tbody>
-                </table>
+                </table>                
             ) : (
-                <p>Nenhum dado encontrado.</p>
+                <p className='not-found'>Nenhum dado encontrado.</p>
             )}
+            <button className='btn'>Cadastrar Personagem</button>
         </div>
     );
 
